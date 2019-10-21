520b49352d460df2fd35392e564a6a7433e421db
Tien Thien <tienthienhd@gmail.com>
2019-10-21 07:32:18 +0700

    gan flnn

---
+++ main.py
@@ -0,0 +1,92 @@
+from data import DataSets
+from run import run
+
+if __name__ == '__main__':
+    dataset = DataSets('data/gg_trace/5.csv',
+                       usecols=[3],
+                       column_names=['cpu'],
+                       header=None,
+                       n_in=1,
+                       n_out=1,
+                       is_diff=True,
+                       feature_range=(-1, 1))
+
+    config_ann = {
+        "model": {
+            "params": {
+                "layer_size": [4, dataset.get_input_shape()[-1]],
+                "activation": 'tanh',
+                "dropout": 0,
+                "output_activation": None
+
+            },
+            "input_shape": dataset.get_input_shape(),
+            "optimizer": 'adam',
+            "learning_rate": 0.001,
+            "model_dir": "test"
+        },
+        "train": {
+            "validation_split": 0.2,
+            "batch_size": 8,
+            "epochs": 100,
+            "verbose": 1,
+            "step_print": 1
+        }
+    }
+
+    config_flnn = {
+        "model": {
+            "params": {
+                "list_function": ['sin', 'cos', 'tan'],
+                "activation": 'tanh',
+                "num_output": 1
+
+            },
+            "input_shape": dataset.get_input_shape(),
+            "optimizer": 'adam',
+            "learning_rate": 0.001,
+            "model_dir": "test"
+        },
+        "train": {
+            "validation_split": 0.2,
+            "batch_size": 8,
+            "epochs": 10,
+            "verbose": 1,
+            "step_print": 1
+        }
+    }
+
+    config_flnn_gan = {
+        "model": {
+            "params_generator": {
+                "list_function": ['sin', 'cos', 'tan'],
+                "activation": 'tanh',
+                "num_output": 1
+
+            },
+            "params_discriminator": {
+                "list_function": ['sin', 'cos', 'tan'],
+                "activation": 'tanh',
+                "num_output": 1
+
+            },
+            "input_shape": dataset.get_input_shape(),
+            "noise_shape": dataset.get_input_shape(),
+            "optimizer_g": 'adam',
+            "optimizer_d": 'adam',
+            "learning_rate_g": 0.001,
+            "learning_rate_d": 0.001,
+            "num_train_d": 1,
+            "is_wgan": False,
+            "model_dir": "logs/flnn_gan"
+        },
+        "train": {
+            "validation_split": 0.2,
+            "batch_size": 8,
+            "epochs": 10,
+            "verbose": 1,
+            "step_print": 1
+        }
+    }
+
+    run('FlnnFlnnGan', config=config_flnn_gan, dataset=dataset)
--- src/model_zoo.py
+++ src/model_zoo.py
@@ -6,7 +6,9 @@ import utils
 __all__ = [
     "AnnModel",
     "AnnGan",
-    "FlnnModel"
+    "FlnnModel",
+    "FlnnFlnnGan"
+
     # "LstmGan",
     # "GruGan",
     # "LstmCnnGan"
@@ -18,6 +20,7 @@ class AnnModel(RegressionModel):
         net = DenseNet(params, 'dense_net')
         optimizer = utils.get_optimizer(optimizer, learning_rate)
         super().__init__(net, input_shape=input_shape, optimizer=optimizer, model_dir=model_dir)
+
 
 class FlnnModel(RegressionModel):
     def __init__(self, params, input_shape, optimizer, learning_rate, model_dir='logs/flnn'):
@@ -41,6 +44,29 @@ class AnnGan(GanModel):
                  model_dir='logs/ann_gan'):
         generator = DenseNet(params_generator, 'generator')
         discriminator = DenseNet(params_discriminator, 'discriminator')
+
+        optimizer_g = utils.get_optimizer(optimizer_g, learning_rate_g)
+        optimizer_d = utils.get_optimizer(optimizer_d, learning_rate_d)
+
+        super().__init__(generator, discriminator, input_shape, noise_shape, optimizer_g, optimizer_d, num_train_d,
+                         model_dir, is_wgan)
+
+
+class FlnnFlnnGan(GanModel):
+    def __init__(self,
+                 params_generator,
+                 params_discriminator,
+                 input_shape,
+                 noise_shape,
+                 optimizer_g,
+                 optimizer_d,
+                 learning_rate_g,
+                 learning_rate_d,
+                 num_train_d,
+                 is_wgan=False,
+                 model_dir='logs/ann_gan'):
+        generator = FlnnNet(params_generator, 'generator')
+        discriminator = FlnnNet(params_discriminator, 'discriminator')
 
         optimizer_g = utils.get_optimizer(optimizer_g, learning_rate_g)
         optimizer_d = utils.get_optimizer(optimizer_d, learning_rate_d)
