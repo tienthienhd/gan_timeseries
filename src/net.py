9f9b733aac0bb37473b028095cbcd68981218f1a
tienthien <tienthienhd@gmail.com>
2019-10-21 13:38:54 +0700

    combine rnn net

--- src/net.py
+++ src/net.py
@@ -6,7 +6,7 @@ class Net:
         self.params = params
         self.scope = scope
 
-    def __call__(self, x, reuse=False, *args, **kwargs):
+    def __call__(self, x, z=None, reuse=False, *args, **kwargs):
         pass
 
 
@@ -19,10 +19,12 @@ class DenseNet(Net):
         self.dropout = params['dropout']
         self.output_activation = params['output_activation']
 
-    def __call__(self, x, reuse=False, *args, **kwargs):
+    def __call__(self, x, z=None, reuse=False, *args, **kwargs):
         with tf.variable_scope(self.scope) as scope:
             if reuse:
                 scope.reuse_variables()
+            if z is not None:
+                x = tf.concat([x, z], axis=1)
             net = tf.keras.layers.Flatten()(x)
             for i, units in enumerate(self.layer_size[:-1]):
                 net = tf.keras.layers.Dense(units, self.activation)(net)
@@ -31,45 +33,36 @@ class DenseNet(Net):
         return net
 
 
-class LstmNet(Net):
+class RnnNet(Net):
     def __init__(self, params, scope):
         super().__init__(params, scope)
         self.layer_size = params['layer_size']
         self.activation = params['activation']
         self.dropout = params['dropout']
         self.output_activation = params['output_activation']
+        self.cell_type = self._get_cell(params['cell_type'])
 
-    def __call__(self, x, reuse=False, *args, **kwargs):
+    def _get_cell(self, type_cell):
+        if type_cell == 'lstm':
+            return tf.keras.layers.LSTMCell
+        elif type_cell == 'gru':
+            return tf.keras.layers.GRUCell
+        else:
+            raise NotImplementedError
+
+    def __call__(self, x, z=None, reuse=False, *args, **kwargs):
         with tf.variable_scope(self.scope) as scope:
             if reuse:
                 scope.reuse_variables()
+            if z is not None and kwargs['before']:
+                x = tf.concat([x, z], axis=1)
             cells = []
             for i, units in enumerate(self.layer_size[:-1]):
-                cell = tf.keras.layers.LSTMCell(units, activation=self.activation, dropout=self.dropout)
+                cell = self.cell_type(units, activation=self.activation, dropout=self.dropout)
                 cells.append(cell)
             net = tf.keras.layers.RNN(cells, return_sequences=False)(x)
-            net = tf.keras.layers.Dense(self.layer_size[-1], activation=self.output_activation)(net)
-        return net
-
-
-class GruNet(Net):
-
-    def __init__(self, params, scope):
-        super().__init__(params, scope)
-        self.layer_size = params['layer_size']
-        self.activation = params['activation']
-        self.dropout = params['dropout']
-        self.output_activation = params['output_activation']
-
-    def __call__(self, x, reuse=False, *args, **kwargs):
-        with tf.variable_scope(self.scope) as scope:
-            if reuse:
-                scope.reuse_variables()
-            cells = []
-            for i, units in enumerate(self.layer_size[:-1]):
-                cell = tf.keras.layers.GRUCell(units, activation=self.activation, dropout=self.dropout)
-                cells.append(cell)
-            net = tf.keras.layers.RNN(cells, return_sequences=False)(x)
+            if z is not None and kwargs['after']:
+                net = tf.concat([net, z], axis=1)
             net = tf.keras.layers.Dense(self.layer_size[-1], activation=self.output_activation)(net)
         return net
 
